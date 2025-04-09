f78bb4a7d439f098bb34d5492698219763455cec
Arjawa <arjawabagus14@gmail.com>
2025-04-09 16:38:19 +0800

    add condition is the data empty or not, if empty then give a response to client a message

--- server/controllers/authorController.js
+++ server/controllers/authorController.js
@@ -20,7 +20,12 @@ export const createAuthor = async (req, res) => {
       const authors = await prisma.author.findMany({
         include: { books: true }
       });
+
+      if(authors.length === 0){
+        res.status(404).json({message: "Ga ada author manapun di sini, maaf ya kawan😫"})
+      }
       res.status(200).json(authors);
+      
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
